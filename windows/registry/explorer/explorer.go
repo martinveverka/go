8be427c62b8147b1346dac1298c6b35160741908
Martin Veverka <martin@veverka.eu>
2021-01-12 11:30:30 +0100

    Update explorer.go

--- windows/registry/explorer/explorer.go
+++ windows/registry/explorer/explorer.go
@@ -17,11 +17,11 @@ const (
 	DOWNLOADS = `{374DE290-123F-4565-9164-39C4925E467B}`
 	MUSIC = `My Music`
 	PICTURES = `My Pictures`
-	VIDEO = `My Video`
 	RECENT = `Recent`
 	SEND_TO = `SendTo`
 	START_MENU = `Start Menu`
 	STARTUP = `Startup`
+	VIDEO = `My Video`
 )
 
 func GetUserShellFolder(key string) (string, error) {
