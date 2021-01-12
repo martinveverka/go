b16e79bb40598bf864510cadc3ea045581a3e73c
Martin Veverka <martin@veverka.eu>
2021-01-12 11:40:00 +0100

    Update explorer.go

--- windows/registry/explorer/explorer.go
+++ windows/registry/explorer/explorer.go
@@ -9,19 +9,19 @@ import (
 
 const (
 	// User Shell Folders registry keys
-	APPDATA = `AppData`
-	APPDATA_LOCAL = `Local AppData`
-	CAPTURES = `{EDC0FE71-98D8-4F4A-B920-C8DC133CB165}`
-	DESKTOP = `Desktop`
-	DOCUMENTS = `Personal`
-	DOWNLOADS = `{374DE290-123F-4565-9164-39C4925E467B}`
-	MUSIC = `My Music`
-	PICTURES = `My Pictures`
-	RECENT = `Recent`
-	SEND_TO = `SendTo`
-	START_MENU = `Start Menu`
-	STARTUP = `Startup`
-	VIDEO = `My Video`
+	AppData      = `AppData`
+	AppDataLocal = `Local AppData`
+	Captures     = `{EDC0FE71-98D8-4F4A-B920-C8DC133CB165}`
+	Desktop      = `Desktop`
+	Documents    = `Personal`
+	Downloads    = `{374DE290-123F-4565-9164-39C4925E467B}`
+	MyMusic      = `My Music`
+	MyPictures   = `My Pictures`
+	MyVideo      = `My Video`
+	Recent       = `Recent`
+	SendTo       = `SendTo`
+	StartMenu    = `Start Menu`
+	Startup      = `Startup`
 )
 
 func GetUserShellFolder(key string) (string, error) {
